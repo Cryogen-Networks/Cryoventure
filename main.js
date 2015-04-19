3891dbe986ff1211b84c2687c6eccfbabfc493e1
BuildTools <unconfigured@null.spigotmc.org>
2015-04-19 15:45:04 +0800

    First commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function init() {
+    function Character () = {
+        this.name = prompt('Choose a name for your character.');;
+        this.inventory = [];
+        this.outfit = {
+            helm: null;
+            body: null;
+            legs: null;
+            feet: null;
+        }
+        this.position =
+    }
+}
