c385e2cddd9f64e4343170b3ce2ed995aa3d5bfc
EzraEn1 <ezraen@gmail.com>
2015-04-20 08:03:50 +0800

    Add more things for the user to see.

--- main.js
+++ main.js
@@ -12,12 +12,13 @@ function Character() {
 }
 //Visuals
 function show(p) {
-    document.write("<p>" + p + "</p>");
+    var container = document.getElementById('container'), content ="<p>" + p + "</p>";
+    container.insertAdjacentHTML('beforeend', content);
 }
 //Game instantiation
 function init() {
     var player = new Character();
-    show("Welcome to Cryogen, " + player.name + "!\nYou are now starting on your adventure..\n");
+    show("Welcome to Cryogen, " + player.name + "!\nYou are now starting on your adventure..");
     show("----------------------------------------");
     show("You wake to the sound of horses neighing.. and blurred vison.\n[Shake your head!]");
 }
