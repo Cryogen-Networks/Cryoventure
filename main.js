91725120e6074844dccbe7c44e174bbbc3d7988a
EzraEn1 <ezraen@gmail.com>
2015-04-20 19:51:06 +0800

    Add more stuff.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+debugger;
 //Character Constructor
 function Character() {
-    this.name = prompt("Choose a name for your character.");
+    this.name = input("Choose a name for your character.", "");
     this.inventory = [];
     this.outfit = {
         helm: null,
@@ -10,16 +11,37 @@ function Character() {
     };
     this.action = [];
 }
+//Variables
+var response, out, content, player;
+//Input
+function input(command, output) {
+    //Response
+    response = document.getElementById('in');
+    if (response.innerHTML == command) {
+        show(output);
+    } else if (response.innerHTML === isNaN) {
+        input(command, output);
+    } else {
+        show('I don\'t think that\'s a valid command. Try another variant, or another command.');
+    }
+}
 //Visuals
-function show(p) {
-    var container = document.getElementById('container'), content ="<p>" + p + "</p>";
-    container.insertAdjacentHTML('beforeend', content);
+function show(p, intro) {
+    out = document.getElementById('out');
+    content = "<p>" + p + "</p>";
+    if (intro) {
+        out.innerHTML = content;
+    } else {
+        out.insertAdjacentHTML('beforeend', content);
+    }
 }
 //Game instantiation
 function init() {
-    var player = new Character();
+    player = new Character();
     show("Welcome to Cryogen, " + player.name + "!\nYou are now starting on your adventure..");
-    show("----------------------------------------");
+    show("--------------------------------------------------");
     show("You wake to the sound of horses neighing.. and blurred vison.\n[Shake your head!]");
+    input('shake head', 'You shake your head, and your vision clears.\n');
 }
+//Starting..
 init();
